9699f33b84ff40624a77f04f4e29fd647cbc2560
imamiya-masaki <imamiyagit@gmail.com>
2023-03-04 19:23:03 +0900

    iconのimport を個別にする

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,18 +1,42 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
+import {FaArrowLeft,FaArrowRight, FaShoppingCart, FaUser, FaPlay, FaCheckCircle} from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
+const iconTypes = ['FaArrowLeft','FaArrowRight', 'FaShoppingCart', 'FaUser', 'FaPlay', 'FaCheckCircle'] as const
+type IconType = typeof iconTypes[number]
+
 type Props = {
-  type: keyof typeof Icons;
+  type: IconType;
   width: number;
   height: number;
   color: string;
 };
 
+
 export const Icon: FC<Props> = ({ color, height, type, width }) => {
-  const Icon = Icons[type];
+  let Icon = FaArrowLeft;
+  switch(type) {
+    case 'FaArrowLeft':
+      Icon = FaArrowLeft;
+      break;
+    case 'FaArrowRight':
+      Icon = FaArrowRight;
+      break;
+    case 'FaCheckCircle':
+      Icon = FaCheckCircle;
+      break;
+    case 'FaPlay':
+      Icon = FaPlay;
+      break;
+    case 'FaShoppingCart':
+      Icon = FaShoppingCart;
+      break;
+    case 'FaUser':
+      Icon = FaUser;
+      break;
+  }
   return (
     <span className={classNames(type, styles.container({ color, height, width }))}>
       <Icon />
